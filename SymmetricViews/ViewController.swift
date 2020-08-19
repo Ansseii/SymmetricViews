2dfccd42b048af1769df3c90343b46f0fb0bebe5
Aleksandr Lukash <a.lukash.92@gmail.com>
2020-08-19 20:43:44 +0300

    Initial Commit

---
+++ SymmetricViews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SymmetricViews
+//
+//  Created by Aleksandr on 19.08.2020.
+//  Copyright © 2020 Aleksandr Lukash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
